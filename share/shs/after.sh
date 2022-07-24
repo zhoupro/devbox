6f3c95185d68cdaf58adf273734789506ef0ec4e
prozhou <zhoushengzheng@gmail.com>
2022-07-24 18:08:46 +0800

    feat: fix

--- share/shs/after.sh
+++ share/shs/after.sh
@@ -41,7 +41,7 @@ EOF
 cat <<EOF > ~/.config/nvim/after/plugin/nvim-lspconfig.lua
 
 require("nvim-lsp-installer").setup {
-  automatic_installation = true
+  automatic_installation = false
 }
 
 local lspconfig = require("lspconfig")
@@ -117,7 +117,7 @@ lspconfig.sumneko_lua.setup {
 
 
 -- Enable some language servers with the additional completion capabilities offered by nvim-cmp
-local servers = { 'gopls', 'pyright','clangd','phan' }
+local servers = { 'gopls', 'pyright','clangd','phpactor' }
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
     on_attach = on_attach,
