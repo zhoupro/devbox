74d80c456a2ea5abe45cf00f32feaad6fd6ccd69
prozhou <zhoushengzheng@gmail.com>
2023-01-08 10:04:43 +0800

    feat: react

--- share/shs/neovim_after.sh
+++ share/shs/neovim_after.sh
@@ -182,7 +182,7 @@ lspconfig.sumneko_lua.setup {
 
 
 -- Enable some language servers with the additional completion capabilities offered by nvim-cmp
-local servers = { 'gopls', 'pyright','clangd','phpactor', 'bashls','awk_ls','html','sqls','volar' }
+local servers = { 'gopls', 'pyright','clangd','phpactor', 'bashls','awk_ls','html','sqls','volar','tsserver' }
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
     on_attach = on_attach,
